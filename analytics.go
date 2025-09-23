fa84ac94e566e22ba2a8f771b4bf6b3c09c5ce68
Sai Kumar Battinoju <88789928+saikumarrs@users.noreply.github.com>
2025-09-23 13:20:12 +0530

    chore: release v4.2.2 (#41)

--- analytics.go
+++ analytics.go
@@ -18,7 +18,7 @@ import (
 )
 
 // Version of the client.
-const Version = "4.2.1"
+const Version = "4.2.2"
 
 // This interface is the main API exposed by the analytics package.
 // Values that satsify this interface are returned by the client constructors
